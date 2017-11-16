7881da313a586eea666f19915b39f1654bfa668f
Mark Salisbury <mark.salisbury@hp.com>
2017-11-16 16:27:07 +0900

    Switch libepoxy to CMake based build
    - So I can get epoxy to link with EGL (ANGLE).
    - If someone else can make epoxy + angle + meson work, let me know.
    - fix initialization error

--- ports/libepoxy/portfile.cmake
+++ ports/libepoxy/portfile.cmake
@@ -1,23 +1,37 @@
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    message(STATUS "Warning: Static building not supported yet. Building dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
+#if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+#    message(STATUS "Warning: Static building not supported yet. Building dynamic.")
+#    set(VCPKG_LIBRARY_LINKAGE dynamic)
+#endif()
 
 include(vcpkg_common_functions)
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO anholt/libepoxy
-    REF 1.4.3
-    SHA512 41c7a4eea66c89346b0ec71407b2d22bf645ed0ef81ebad560370903f138ed48abb6bc6bcc88c75a3a05497acc6720397db828d61301599c05040263a9f4f7f0
+    REPO https://github.com/masali-hp/libepoxy
+    REF 56eba0d55334dc36aca20e69c68c54380e6e905e
     HEAD_REF master)
 
-vcpkg_configure_meson(SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS 
-        -Denable-glx=no 
-        -Denable-egl=no)
-vcpkg_install_meson()
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    SET(EPOXY_LIB_TYPE "SHARED")
+else()
+    SET(EPOXY_LIB_TYPE "STATIC")
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${UWP_OPTIONS}
+        -DENABLE_GLX=0
+        -DENABLE_EGL=1
+        -DENABLE_WGL=1
+        -DEPOXY_LIB_TYPE=${EPOXY_LIB_TYPE}
+    OPTIONS_DEBUG
+        -DENABLE_DEBUG=ON
+)
+
+vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/pkgconfig)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share/pkgconfig)
 
