95f8fb05988cc39ab401ed23343a1d8d0e3fd87e
Mark Salisbury <mark.salisbury@hp.com>
2018-01-08 13:51:13 -0700

    Add wpebackend-rdk

---
+++ ports/wpebackend-rdk/portfile.cmake
@@ -0,0 +1,33 @@
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    message(STATUS "Warning: Static building not supported yet. Building dynamic.")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+include(vcpkg_common_functions)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO masali-hp/WPEBackend-rdk
+    REF windows-backend
+    SHA512 05829debd28e959a295721b50b5d3b79e391e66e2407ca9178baba00e4deeedfc222b5823a7fc5d0e0cb20bd5115c19838ef34f97600f917f90ccbdf935d37e6
+    HEAD_REF master)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${UWP_OPTIONS}
+    OPTIONS_DEBUG
+        -DENABLE_DEBUG=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/wpebackend-rdk)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/wpebackend-rdk/COPYING ${CURRENT_PACKAGES_DIR}/share/wpebackend-rdk/copyright)
+
+# Allow empty include directory
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+# Allow no import lib
+SET(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
